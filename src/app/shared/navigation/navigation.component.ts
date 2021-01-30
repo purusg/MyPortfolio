3157c0c5d1944f28ed25edff08152396b3f86e46
Purushothaman Govindaraj <PUGOV@orsted.dk>
2021-01-30 20:02:57 +0800

    new portfolio added

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-navigation',
@@ -13,10 +14,23 @@ export class NavigationComponent implements OnInit {
 
   ngOnInit() {
     this.showFiller = true;
+    this.typedInit();
   }
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
   }
 
+  typedInit() {
+    const options = {
+      strings: ['Senior .Net Engineer', 'Certified Azure Developer', 'Full Stack Developer (.Net)'],
+      typeSpeed: 100,
+      backSpeed: 40,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true
+    };
+
+    const typed = new Typed('.typed-element', options);
+  }
 }
